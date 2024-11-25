2610762928964363448794e4ca2b0431adff69db
ROUSSYCharles <roussy.charles21@gmail.com>
2024-11-25 08:58:18 +0100

    recuperation des donnees du formulaire produit ok

--- src/Controller/ManageProductController.php
+++ src/Controller/ManageProductController.php
@@ -6,16 +6,23 @@ use App\Entity\Product;
 use App\Form\Type\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 class ManageProductController extends AbstractController
 {
     #[Route('/manage/product/new', name: 'manage_product_new')]
-    public function new(): Response {
+    public function new(Request $request): Response {
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
 
         $form->add('Ajouter', SubmitType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            dd($product);
+        }
+
         return $this->renderForm('product/product_new.html.twig', [
             'form' => $form,
         ]);
